e23bb9d80bff09ec5106c2430393a0cc6342b986
YunHyuck <39765493+yunhyuck@users.noreply.github.com>
2019-04-06 13:48:13 +0900

    Functionless java
    Functionless

---
+++ 02.Code/02.Hadoop/WordCountMapper.java
@@ -0,0 +1,9 @@
+/**
+ * @WordCountMapper
+ * @Title: 맵 클래스(raw 데이터를 가져와 key와 value로 바꾸는 작업 수행)
+ * @author: Lee yun Hyuck
+ * @Create_at: 2019-04-06
+ */
+public class WordCountMapper {
+
+}
---
+++ 02.Code/02.Hadoop/WordCountReducer.java
@@ -0,0 +1,9 @@
+/**
+ * @WordCountReducer
+ * @Title: 리듀스 클래스(맵에서 선별한 데이터(key, value)를 같은 키를 기준으로 더하는 작업 수행 )
+ * @author: Lee yun Hyuck
+ * @Create_at: 2019-04-06
+ */
+public class WordCountReducer {
+
+}
