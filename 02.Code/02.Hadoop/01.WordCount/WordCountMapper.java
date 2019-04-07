eba6311b4fd1b59038d3d9e61a57af1e68469030
YunHyuck <39765493+yunhyuck@users.noreply.github.com>
2019-04-07 18:27:49 +0900

    Update WordCountMapper.java

--- 02.Code/02.Hadoop/01.WordCount/WordCountMapper.java
+++ 02.Code/02.Hadoop/01.WordCount/WordCountMapper.java
@@ -1,7 +1,7 @@
 /**
  * @WordCountMapper
  * @Title: 맵 클래스(raw 데이터를 가져와 key와 value로 바꾸는 작업 수행)
- * @author: Lee yun Hyuck
+ * @author: Lee_yun_Hyuck
  * @Create_at: 2019-04-06
  */
 import java.io.IOException;
