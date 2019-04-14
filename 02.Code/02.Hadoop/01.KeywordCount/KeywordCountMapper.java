d79be4de0ec6d4e9d2b6532e6a437ca6f3c171a0
YunHyuck <39765493+yunhyuck@users.noreply.github.com>
2019-04-15 01:48:01 +0900

    Add files via upload

--- 02.Code/02.Hadoop/01.KeywordCount/KeywordCountMapper.java
+++ 02.Code/02.Hadoop/01.KeywordCount/KeywordCountMapper.java
@@ -3,16 +3,24 @@
  * @Title: 맵 클래스(raw 데이터를 가져와 key와 value로 바꾸는 작업 수행)
  * @author: Lee_yun_Hyuck
  * @Create_at: 2019-04-06
- * @Modify_on: 2019-04-09
- * @text: 주석 추가, 클래스 명 변경
+ * @Modify_on: 2019-04-15
+ * @text: 주석 추가, 클래스 명 변경, 자연어 처리
  */
 import java.io.IOException;
-import java.util.StringTokenizer;
  
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.openkoreantext.processor.OpenKoreanTextProcessorJava;
+import org.openkoreantext.processor.tokenizer.KoreanTokenizer;
+
+import scala.collection.Seq;
+
+import java.util.Iterator;
+import java.util.List;
+
+
 //Mapper 클래스의 generic 타입  <입력키, 입력값, 출력키, 출력값>
 //하둡에서 요구되는 long, int, String에 대응되는 타입으로 변경해서 사용 
 public class KeywordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
@@ -26,14 +34,23 @@ public class KeywordCountMapper extends Mapper<LongWritable, Text, Text, IntWrit
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
             throws IOException, InterruptedException {
-        //공백 단위로 들어온 텍스트를 끊어 온다.
+        //------------------------------------자연어 처리부분-----------------------------------------
+    	//Normalize
+    	CharSequence normalized = OpenKoreanTextProcessorJava.normalize(value.toString());
+    	//Tokenize
+       Seq<KoreanTokenizer.KoreanToken> tokens = OpenKoreanTextProcessorJava.tokenize(normalized);
+       List<String> list_token = OpenKoreanTextProcessorJava.tokensToJavaStringList(tokens);
+       Iterator<String> itr = list_token.iterator();
+       //------------------------------------------------------------------------------------------
+    	
+    	//공백 단위로 들어온 텍스트를 끊어 온다.
     	//DB에서 가져온 데이터는 ,로 구분되어 있기 때문에 replaceAll 명령을 통해 ,를 공백으로 바꾼다.
-    	StringTokenizer itr = new StringTokenizer(value.toString().replaceAll(",", " "));
+    	//StringTokenizer itr = new StringTokenizer(value.toString().replaceAll(",", " "));
         
     	//리턴할 다음 토큰이 없을 때(false) 만큼 반복. 
-    	while(itr.hasMoreTokens()) {
+    	while(itr.hasNext()) {
     		//다음 토큰을 리턴하고, 이전 토큰은 제거 한다.
-    		word.set(itr.nextToken());
+    		word.set(itr.next());
     		//context 객체는 키-값쌍으로 내보낼 때 사용되며, 출력타입으로 인자화 된다.
     		context.write(word, one);
         }
