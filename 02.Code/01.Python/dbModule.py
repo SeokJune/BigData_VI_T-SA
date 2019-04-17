73e3dd5f40b1129ef183fef85c1321adc4b1eb23
BaeInGyu <46333678+BaeInGyu@users.noreply.github.com>
2019-04-17 23:01:11 +0900

    Update dbModule.py

--- 02.Code/01.Python/dbModule.py
+++ 02.Code/01.Python/dbModule.py
@@ -25,12 +25,6 @@ class dbModule :
 		self.charset = charset
 
 
-	'''
-	 Modifier: Bae InGyu
-	Modify on: 2019.04.09.
-	cursor생성 이곳에서 하는걸로 변경 및 DB연결,cursor Return으로 변경  
-	'''
-
 	# MariaDB연결함수----------------------------------------------------------------------------
 	def dbConnect(self) :
 		conn = pymysql.connect(host = self.host, user = self.user, password = self.pswd, db = self.db, charset = self.charset)
@@ -40,68 +34,6 @@ class dbModule :
 
 
 
-	'''
-	 Modifier: Bae InGyu
-	Modify on: 2019.04.05.
-	연결상태 예외처리 추가 
-	
-	# MariaDB연결함수----------------------------------------------------------------------------
-	def dbConnect(self) :
-		conn = pymysql.connect(host = self.host, user = self.user, password = self.pswd, db = self.db, charset = self.charset)
-		if conn.open == True :              
-			print("========DB연결성공========")
-		else :
-			print("========DB연결실패========")
-		# dbModule에서 global 선언하면 dbModule에서만 적용됨 Main.py에서 import해도 적용안됨 
-	'''
-
-
-	'''
- 	 Modifier: Bae InGyu
-	Modify on: 2019.04.03. 
-	MariaDB연결기능 함수선언	
-	
-	class dbModule :
-	host = 'localhost'  # 접속할 mysql server 주소
-	user = 'root'       # 접속할 mysql ID
-	pswd = '1234'       # 접속할 mysql ID의 암호
-	db = 'mysql'        # 접속할 데이터베이스
-	charset = 'utf8'    # mysql에서 select할때 한글이 깨질 수 있으므로 설정
-
-	# MariaDB연결함수
-	def dbconn() :
-		return pymysql.connect(host = dbModule.host, user = dbModule.user, password = dbModule.pswd, db = dbModule.db, charset = dbModule.charset)
-                
-	'''  
-                                     
-	'''
-	Modifier: Bae InGyu
-	Modify on: 2019-04-04.
-	__init__함수추가
- 	
-
-	def __init__(self,host,user,pswd,db,charset):
-		self.host = host         
-		self.user = user
-		self.pswd = pswd       
-		self.db = db        
-		self.charset = charset
-
-	# MariaDB연결함수
-	def dbconn(self):
-		global conn 
-		conn = pymysql.connect(host = self.host, user = self.user, password = self.pswd, db = self.db, charset = self.charset)
-		print("DB연결 완료")
-	'''
-	#===========================================================================================#
-
-
-	'''
-	 Modifier: Bae InGyu
-	Modify on: 2019.04.09.
-	cusror종료 여기서 하는걸로 변경 및 DB연결종료cursor종료 Return으로 변경 
-	'''
-
 	# MariaDB연결종료함수-------------------------------------------------------------------------
 	def dbClose(self) :
 		conn , curs = self.dbConnect()
@@ -109,60 +41,52 @@ class dbModule :
 
 
 	'''
-	 Modifier: Bae InGyu
-	Modify on: 2019.04.05.
-	연결상태 예외처리 추가 
-	
-	# MariaDB연결종료함수-------------------------------------------------------------------------
-	def dbClose(self) :
-		conn.close()
-		if conn.open == True :                            
-			print("========DB연결성공========")
-		else :
-			print("========DB연결종료========")
+ 	 Modifier: Bae InGyu
+	Modify on: 2019-04-17 
+	기존의 하나의 테이블만 ROW 여부파악에서 모든 테이블의 ROW 여부파악으로 수정
 	'''
 
+	# 모든테이블에 ROW 여부파악 함수------------------------------------------------------------ 
+	def getRowByCheck(self,tableName) :
+		try :	
+			# MariaDB연결 및 Cursor생성
+			conn, curs = self.dbConnect()
+
+			# 테이블 조회
+			result = []
+			i = 0
+			for table in tableName :
+				sql = "select * from "+table.strip()+";"
+				curs.execute(sql)
+        
+				# 테이블 데이터출력
+				result.append(curs.fetchall())		
+				print(result[i])
+				i += 1
+				
+			# 모든 테이블 ROW 여부파악 하나의 테이블이라도 ROW가 존재하면 True로 반환
+			if bool(result[0] or result[1] or result[2] or result[3] or result[4]) == True :
+				return True
+			
+		except :
+			print("파악실패")
+		
+		finally :
+			# Select후 Cursor종료 및 MariaDB연결종료                
+			self.dbClose()
+
+
+
 	'''
-	 Modifier: Bae In Gyu
-	Modify on: 2019.04.04.
-	MariaDB연결종료함수추가	
-
-	# MariaDB연결종료함수
-	def dbclose(self) :
-                conn.close()
-                print("DB연결 종료")
-	'''
-	#===========================================================================================#
-
-
-
-	'''
-	 Modifier: Bae InGyu
-	Modify on: 2019.04.05.
-	테이블의 Row 존재여부 파악 함수추가 
-	'''
-
-	# 해당 테이블에 레코드 존재파악 함수------------------------------------------------------------ 
 	def getRowByCheck(self,table) :
                	# 데이터가 없으면 false 있으면 True
 		if bool(self.selectDB(table)) == True :
 			return True 
 		else : 
 			return False
-	
-	# found_rows()(직전 select 한 테이블 row수를 반환) 
-	# return conn.cursor().rowcount (select 후 사용하면 -1을 리턴함)
-	# UDATE/ DELETE/ INSERT 시에는 실제로 적용된 Row 수를 얻기위해서는 ROW_COUNT 를 사용한다.
-
-	#===========================================================================================#
-	
-
-
 	'''
-	 Modifier: Bae InGyu
-	Modify on: 2019-04-06 
-	try ~ fianlly 추가
-	'''
+
+
          
 	# 테이블의 데이터 모두조회함수----------------------------------------------------------------
 	def selectDB(self,table) :
@@ -187,98 +111,36 @@ class dbModule :
 			self.dbClose()
                  		
                     
-
 	'''
-	 Modifier: Bae InGyu
-	Modify on: 2019-04-03
-	select기능 함수선언
- 
-	#Select 함수
-	def select (table) :
-		# MariaDB연결 및 Cursor 생성
-		conn = dbModule.dbconn()
-		curs = conn.cursor()
-		
-		# 테이블 조회
-		select = "select * from "+table;
-		curs.execute(select)
-		
-		# 테이블 데이터출력
-		rows = curs.fetchall()
-		for data in rows :
-			print(data)
-                
-		# Select종료시 MariaDB연결종료
-		conn.close()
-
+ 	 Modifier: Bae InGyu
+	Modify on: 2019-04-17 
+	기존의 하나의 테이블의 ROW만 삭제에서 모든 테이블의 ROW 삭제로 변경
 	'''
 
-	'''
-	 Modifier: Bae In Gyu
-	Modify on: 2019-04-04 Code
-	결과값 return result로 변경
 
-	# 테이블의 데이터 모두조회
-	def selectDB (self,table) :
-		# MariaDB연결 및 Cursor생성
-                dbModule_3.dbconn(self)
-                curs = conn.cursor()
+	# 모든테이블의 데이터 모두삭제함수----------------------------------------------------------------	
+	def deleteDB (self,tableName) :
+		try : 
+			# MariaDB연결 및 Cursor생성
+			conn, curs = self.dbConnect()
 
-		# 테이블 조회
-                sql = "select * from "+table+";"
-                curs.execute(sql)
+			# Data삭제
+			for table in tableName :
+				sql = "delete from "+table.strip()+";"              
+				curs.execute(sql)
+				conn.commit()
+				print("삭제완료")
 
+		except :
+			print("삭제실패")		 
 
-		# 테이블 데이터출력	
-                rows = curs.fetchall()
-                result = rows
-		
-		# Select종료시 MariaDB연결종료
-                dbModule_3.dbclose(self)
-		
-                print("조회완료")
-                return result
-
-	'''
-
-	'''
-	 Modifier: Bae In Gyu
-	Modify on: 2019-04-05 Code
-	Cursor()종료추가 , 테이블명 입력시 양쪽끝 공백있을시 제거기능 추가
-	
-	# 테이블의 데이터 모두조회함수
-	def selectDB(self,table) :
-		  # MariaDB연결 및 Cursor생성
-                  self.dbConnect()
-                  curs = conn.cursor()
-
-		  # 테이블 조회
-                  sql = "select * from "+table.strip()+";"
-                  curs.execute(sql)
-
-		  # 테이블 데이터출력	
-                  rows = curs.fetchall()
-
-                  result = rows
-		
-		  # Select후 Cursor종료 및 MariaDB연결종료
-                  curs.close()                 
-                  self.dbClose()
-                 		
-                  return result
-
-	'''
-	#===========================================================================================#
-
+		finally :
+			# Cursor종료 및 MariaDB연결종료 
+			self.dbClose()
+                 
 
 
 	'''
-	Modifier: Bae In Gyu
-	Modify on: 2019-04-06 
-	try ~ fianlly 추가
-	'''
-
-	# 테이블의 데이터 모두삭제함수----------------------------------------------------------------	
 	def deleteDB (self,table) :
 		try : 
 			# MariaDB연결 및 Cursor생성
@@ -296,74 +158,19 @@ class dbModule :
 		finally :
 			# Cursor종료 및 MariaDB연결종료 
 			self.dbClose()
-                 
-
-	'''
- 	Modifier: Bae In Gyu
-	Modify on: 2019-04-03 Code
-	delete기능 함수선언
-
-	#Delete	
-	def delete (table) :
-		# MariaDB연결 및 Cursor 생성
-		conn = dbModule.dbconn()
-		curs = conn.cursor()
-
-		# Data삭제
-		delete = "delete from "+table
-		curs.execute(delete)
-		conn.commit()
-
-		# Connection 닫기
-                conn.close()
-
 	'''
 
-	'''
-	 Modifier: Bae In Gyu
-	Modify on: 2019-04-05 Code
-	Cursor()종료추가 , 테이블명 입력시 양쪽끝 공백있을시 제거기능 추가
 
-	# 테이블의 데이터 모두삭제함수(예외처리추가예정)	
-	def deleteDB (self,table) :
-		# MariaDB연결 및 Cursor생성
-                self.dbConnect()
-                curs = conn.cursor()
-
-		# Data삭제
-                sql = "delete from "+table.strip()+";"              
-                curs.execute(sql)
-                conn.commit()
-
-		# Connection 닫기
-                curs.close() 
-                self.dbClose()
-
-                print("삭제완료")
-
-	# insert 함수 추가 예정
-	#def insertDB (table) :
-
-	'''
-	#===========================================================================================#
-
-
-
-	'''
-	Modifier: Bae In Gyu
-	Modify on: 2019-04-06 
-	insertDB 함수추가
-	'''
 
 	# 테이블의 데이터 삽입함수--------------------------------------------------------------------
-	def insertDB (self,table,values) :
+	def insertDB (self,test,values) :
 		try :
 			# MariaDB연결 및 Cursor생성
 			conn, curs = self.dbConnect()
 
-			# Data삽입			
-			self.values = values 
-			sql = "insert into "+table.strip()+values
+			# Data삽입
+			self.values = values			
+			sql = "insert into "+table.strip()+"values"+values+";"
 			curs.execute(sql)
 			conn.commit()
 			print("삽입완료")
@@ -376,19 +183,3 @@ class dbModule :
 			self.dbClose()
 
 
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-	
