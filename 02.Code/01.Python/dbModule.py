303d038f7bb6f44baa9e194c0659f5e7d99048b1
BaeInGyu <46333678+BaeInGyu@users.noreply.github.com>
2019-05-06 16:07:24 +0900

    Update dbModule.py

--- 02.Code/01.Python/dbModule.py
+++ 02.Code/01.Python/dbModule.py
@@ -24,27 +24,16 @@ class dbModule :
 		self.db = db        
 		self.charset = charset
 
-
 	# MariaDB연결함수----------------------------------------------------------------------------
 	def dbConnect(self) :
 		conn = pymysql.connect(host = self.host, user = self.user, password = self.pswd, db = self.db, charset = self.charset)
 		curs = conn.cursor()
 		return conn , curs 
 
-
-
-
 	# MariaDB연결종료함수-------------------------------------------------------------------------
 	def dbClose(self) :
 		conn , curs = self.dbConnect()
 		return conn.close() , curs.close()
-
-
-	'''
- 	 Modifier: Bae InGyu
-	Modify on: 2019-04-17 
-	기존의 하나의 테이블만 ROW 여부파악에서 모든 테이블의 ROW 여부파악으로 수정
-	'''
 
 	# 모든테이블에 ROW 여부파악 함수------------------------------------------------------------ 
 	def getRowByCheck(self,tableName) :
@@ -65,7 +54,7 @@ class dbModule :
 				i += 1
 				
 			# 모든 테이블 ROW 여부파악 하나의 테이블이라도 ROW가 존재하면 True로 반환
-			if bool(result[0] or result[1] or result[2] or result[3] or result[4]) == True :
+			if bool(result[0] or result[1] or result[2] or result[3] or result[4] or result[5]) == True :
 				return True
 			
 		except :
@@ -75,19 +64,6 @@ class dbModule :
 			# Select후 Cursor종료 및 MariaDB연결종료                
 			self.dbClose()
 
-
-
-	'''
-	def getRowByCheck(self,table) :
-               	# 데이터가 없으면 false 있으면 True
-		if bool(self.selectDB(table)) == True :
-			return True 
-		else : 
-			return False
-	'''
-
-
-         
 	# 테이블의 데이터 모두조회함수----------------------------------------------------------------
 	def selectDB(self,tableName) :
 		try :
@@ -98,8 +74,7 @@ class dbModule :
 			curs.execute(sql)
 
 			# 테이블 데이터출력	
-			rows = curs.fetchall()
-			result = rows
+			result = curs.fetchall()
 
 			return result
 
@@ -109,14 +84,6 @@ class dbModule :
 		finally :
 			# Select후 Cursor종료 및 MariaDB연결종료                
 			self.dbClose()
-                 		
-                    
-	'''
- 	 Modifier: Bae InGyu
-	Modify on: 2019-04-17 
-	기존의 하나의 테이블의 ROW만 삭제에서 모든 테이블의 ROW 삭제로 변경
-	'''
-
 
 	# 모든테이블의 데이터 모두삭제함수----------------------------------------------------------------	
 	def deleteDB (self,tableName) :
@@ -137,56 +104,73 @@ class dbModule :
 		finally :
 			# Cursor종료 및 MariaDB연결종료 
 			self.dbClose()
-                 
-
-
-	'''
-	def deleteDB (self,table) :
-		try : 
-			# MariaDB연결 및 Cursor생성
-			conn, curs = self.dbConnect()
-
-			# Data삭제
-			sql = "delete from "+table.strip()+";"              
-			curs.execute(sql)
-			conn.commit()
-			print("삭제완료")
-
-		except :
-			print("삭제실패")		 
-
-		finally :
-			# Cursor종료 및 MariaDB연결종료 
-			self.dbClose()
-	'''
-
-
 
 	# 테이블의 데이터 삽입함수--------------------------------------------------------------------
 	def insertDB (self, tableName, values) :
-		#try :
+		try :
 			# MariaDB연결 및 Cursor생성
 			conn, curs = self.dbConnect()
 			
 			# Data삽입
-			sql = "insert into " + tableName.strip() + " values("
-			sql += "'" + values[0] + "',"
-			sql +=       str(values[2][0]) + "," 
-			sql +=       str(values[2][1]) + ","
-			sql += "'" + values[1] + "');"
-			curs.execute(sql)
-			conn.commit()
-			print("삽입완료")
 
-		#except :
-		#	print("삽입실패")
-		#	print(valuse[0])
-		#	print(valuse[2][0])
-		#	print(valuse[2][1])
-		#	print(valuse[1])
+			if tableName == 'KEYWORD_JSON':
+				sql = "insert into " + tableName.strip() + " values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+				curs.execute(sql,(values[0],values[2],values[3],values[4],values[5],
+						  values[6],values[11],values[19],values[20],values[23]))
+				conn.commit()
+				print("삽입완료")
 
-		#finally :
+			elif tableName == 'KEYWORD_HASHTAG':			
+				sql = "insert into " + tableName.strip() + " values("
+				sql += "'" + str(values[0]) + "',"
+				sql +=       str(values[2][0]) + "," 
+				sql +=       str(values[2][1]) + ","
+				sql += "'" + str(values[1]) + "');"
+				curs.execute(sql)
+				conn.commit()
+				print("삽입완료")
+
+
+			#else if tableName == 'KEYWORD_METADATA':			
+			#	sql = "insert into " + tableName.strip() + " values("
+
+			#else if tableName == 'KEYWORD_USER':			
+			#	sql = "insert into " + tableName.strip() + " values("
+
+			#else if tableName == 'KEYWORD':			
+			#	sql = "insert into " + tableName.strip() + " values("
+
+			#else if tableName == 'HASHTAG':			
+			#	sql = "insert into " + tableName.strip() + " values("
+			
+
+
+
+		except :
+			print("삽입실패")
+
+		finally :
 			#Cursor종료 및 MariaDB연결종료
-		#	self.dbClose()
+			self.dbClose()
 
+	# Keyword Date Count 함수--------------------------------------------------------------------
+	def keywordDateCount (self, tableName):
+		try : 
+			# MariaDB연결 및 Cursor생성
+			conn, curs = self.dbConnect()
 
+			# Keyword Date Count(Date 수정하고 sql문 수정해야함)
+			sql = "select HCODE, COUNT(*) from " + tableName.strip() + " group by HCODE;"              
+			curs.execute(sql)
+			
+			result = curs.fetchall()
+
+			return result
+			print("Date Count 조회")
+
+		except :
+			print("Date Count 조회실패")		 
+
+		finally :
+			# Cursor종료 및 MariaDB연결종료 
+			self.dbClose()
