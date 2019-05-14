c5ea89c0417aa3a8703ba570416462f3c7008b9e
YunHyuck <39765493+yunhyuck@users.noreply.github.com>
2019-05-14 16:23:41 +0900

    Update KeywordCount.java

--- 02.Code/02.Hadoop/01.KeywordCount/KeywordCount.java
+++ 02.Code/02.Hadoop/01.KeywordCount/KeywordCount.java
@@ -18,16 +18,24 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class KeywordCount {
 	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
+            // Create the conf object needed for Hadoop implementation (hdfs-site, core-site)	
+	    Configuration conf = new Configuration();
+	    // Create job object for job execution
 	    Job job = Job.getInstance(conf, "Keywordcount");
+	    // Specify the library file to be used for job execution
 	    job.setJarByClass(KeywordCount.class);
+	    // Set the classes to use in the job
 	    job.setMapperClass(KeywordCountMapper.class);
 	    job.setCombinerClass(KeywordCountReducer.class);
 	    job.setReducerClass(KeywordCountReducer.class);
+	    // Set key and value type of output data of mapper and reducer class
 	    job.setOutputKeyClass(Text.class);
 	    job.setOutputValueClass(IntWritable.class);
+	    // Set the I / O data path.
+	    // The first parameter is the input parameter, the second parameter is the output parameter
 	    FileInputFormat.addInputPath(job, new Path(args[0]));
 	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	    // If the same values as the path needed for execution are entered
 	    System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
