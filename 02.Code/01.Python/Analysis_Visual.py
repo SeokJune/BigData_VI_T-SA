c22278e7559e8da045b93b1c6e0621dfd3dc43ad
SeokJune <46333532+SeokJune@users.noreply.github.com>
2019-06-06 21:08:29 +0900

    Update Analysis_Visual.py

--- 02.Code/01.Python/Analysis_Visual.py
+++ 02.Code/01.Python/Analysis_Visual.py
@@ -53,13 +53,15 @@ class Analysis_Visual:
         plt.suptitle(title, fontsize = titleSize)
         # Set Text(Date, Query)
         fromDate = '-'.join([self.date[0][0:4], self.date[0][4:6], self.date[0][6:8]])
-        toDate = '-'.join([self.date[1][0:4], self.date[1][4:6], self.date[1][6:8]])
-        plt.text(10 * 2.54 * 15.5, -10, '(기간: ' + fromDate + ' ~ ' + toDate + ')', ha = 'right', wrap=True, fontsize = 20, color = 'red')
-        plt.text(10 * 2.54 * 15.5, -1, '(쿼리: ' + self.query + ')', ha = 'right', wrap=True, fontsize = 20, color = 'red')
+        toDate = '-'.join([self.date[1][0:4], self.date[1][4:6], self.date[1][6:8]])        if textNum == 1:
+            plt.text(10 * 2.54 * 15.5, -10, '(기간: ' + fromDate + ' ~ ' + toDate + ')', ha = 'right', wrap=True, fontsize = 20, color = 'red')
+            plt.text(10 * 2.54 * 15.5, -1, '(쿼리: ' + self.query + ')', ha = 'right', wrap=True, fontsize = 20, color = 'red')
     # ----------------------------------------------------------------------------------------------
     # Word Cloud with kCountData(TWITTER - KEYWORD_COUNT)
     # ----------------------------------------------------------------------------------------------
     def wordCloud(self, kCountData):
+        # Set plt
+        self.setPlt('19대 대통령 선거 Word Count 결과', 50)
         # Set Word Cloud
         wc = WordCloud(font_path = self.hPath,      # Font Path
                        background_color = 'white',  # Backgroud Color
@@ -72,15 +74,29 @@ class Analysis_Visual:
                    interpolation = 'bilinear')  # 'none', 'nearest', 'bilinear', 'bicubic', etc.
         # Set x,y axis (off - invisible)
         plt.axis('off')
-        # Set plt
-        self.setPlt('19대 대통령 선거 Word Count 결과', 50)
         # Graph Output
         plt.show()
     # ----------------------------------------------------------------------------------------------
-    # Line Graph with sJsonData(TWITTER - S_JSON
+    # Line Graph with sJsonData(TWITTER - S_JSON)
     # ----------------------------------------------------------------------------------------------
     def line(self, sJsonData):
-        print(sJsonData)
+        # Preprocessing
+        data = pd.merge(pd.merge(pd.merge(pd.merge(pd.DataFrame(list(sJsonData[0]), columns = ['DATE','문재인']),
+                                                   pd.DataFrame(list(sJsonData[1]), columns = ['DATE','홍준표']), on = 'DATE'),
+                                          pd.DataFrame(list(sJsonData[2]), columns = ['DATE','안철수']), on = 'DATE'),
+                                 pd.DataFrame(list(sJsonData[3]), columns = ['DATE','유승민']), on = 'DATE'),
+                        pd.DataFrame(list(sJsonData[4]), columns = ['DATE','심상정']), on = 'DATE')
+        # Set plt
+        self.setPlt('19대 대통령 선거기간(2017-04-16 ~ 2017-05-09)동안 일별 후보 언급 횟수', 40, 2)
+        # Set Line
+        plt.plot(data['DATE'], data['문재인'], lw = 2, marker = 'o')
+        plt.plot(data['DATE'], data['홍준표'], lw = 2, marker = 'o')
+        plt.plot(data['DATE'], data['안철수'], lw = 2, marker = 'o')
+        plt.plot(data['DATE'], data['유승민'], lw = 2, marker = 'o')
+        plt.plot(data['DATE'], data['심상정'], lw = 2, marker = 'o')
+        plt.legend(('문재인', '홍준표', '안철수', '유승민', '심상정'), loc = 'upper right')
+        #Graph Output
+        plt.show()
 # --------------------------------------------------------------------------------------------------
 # test
 # https://zzsza.github.io/development/2018/08/24/data-visualization-in-python/
