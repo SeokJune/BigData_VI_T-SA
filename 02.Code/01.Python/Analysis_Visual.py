31912932aa7d1798c894f5f552cccbb1dd204197
SeokJune <46333532+SeokJune@users.noreply.github.com>
2019-06-07 06:21:36 +0900

    Update Analysis_Visual.py

--- 02.Code/01.Python/Analysis_Visual.py
+++ 02.Code/01.Python/Analysis_Visual.py
@@ -7,6 +7,8 @@
 # Library for handing DataFrame
 ## pip3 install pandas
 import pandas as pd
+## pip3 install numpy
+import numpy as np
 # Word Cloud related Module
 ## pip3 install wordcloud
 from wordcloud import WordCloud
@@ -40,20 +42,19 @@ class Analysis_Visual:
         # Path(Hangle Font)
         ## sudo apt-get install fonts-nanum*
         self.hPath = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
+        # Set Hangle Font, size / Graph size
+        font_name = font_manager.FontProperties(fname = self.hPath).get_name()
+        rc('font', family = font_name)
     # ----------------------------------------------------------------------------------------------
     # Set matplotlib Param
     # ----------------------------------------------------------------------------------------------
     def setPlt(self, title, titleSize):
-        # Set Hangle Font, size / Graph size
-        font_name = font_manager.FontProperties(fname = self.hPath).get_name()
-        rc('font', family = font_name)
-        # rc('figure', figsize = (20, 10)) or
-        plt.get_current_fig_manager().full_screen_toggle()
         # Set Title
         plt.suptitle(title, fontsize = titleSize)
         # Set Text(Date, Query)
         fromDate = '-'.join([self.date[0][0:4], self.date[0][4:6], self.date[0][6:8]])
-        toDate = '-'.join([self.date[1][0:4], self.date[1][4:6], self.date[1][6:8]])        if textNum == 1:
+        toDate = '-'.join([self.date[1][0:4], self.date[1][4:6], self.date[1][6:8]])
+        if textNum == 1:
             plt.text(10 * 2.54 * 15.5, -10, '(기간: ' + fromDate + ' ~ ' + toDate + ')', ha = 'right', wrap=True, fontsize = 20, color = 'red')
             plt.text(10 * 2.54 * 15.5, -1, '(쿼리: ' + self.query + ')', ha = 'right', wrap=True, fontsize = 20, color = 'red')
     # ----------------------------------------------------------------------------------------------
@@ -75,6 +76,7 @@ class Analysis_Visual:
         # Set x,y axis (off - invisible)
         plt.axis('off')
         # Graph Output
+        plt.get_current_fig_manager().full_screen_toggle()
         plt.show()
     # ----------------------------------------------------------------------------------------------
     # Line Graph with sJsonData(TWITTER - S_JSON)
@@ -96,6 +98,41 @@ class Analysis_Visual:
         plt.plot(data['DATE'], data['심상정'], lw = 2, marker = 'o')
         plt.legend(('문재인', '홍준표', '안철수', '유승민', '심상정'), loc = 'upper right')
         #Graph Output
+        plt.get_current_fig_manager().full_screen_toggle()
+        plt.show()
+    # ----------------------------------------------------------------------------------------------
+    # Bar Graph with sJsonData(TWITTER - KEYWORD_COUNT)
+    # ----------------------------------------------------------------------------------------------
+    def bar(self, kCountData, stat = 0):
+        # Preprocessing
+        data = pd.DataFrame(list(kCountData), columns = ['KEYWORD', 'COUNT']).sort_values(['COUNT'], ascending = [False])
+        if stat == 1:
+            data = data.head(10)
+        else:
+            data = data[data['KEYWORD'].isin(['문재인', '홍준표', '안철수', '유승민', '심상정'])]
+            data = pd.merge(data.sort_values(['KEYWORD']),
+                            np.transpose(self.realVoteData)[0]['문재인':'심상정'].sort_index(),
+                            left_on = ['KEYWORD'], right_index = True)
+            data = data.rename(columns = {0 : 'COUNT_'})
+            data['COUNT_'] = data['COUNT_'].replace(',', '', regex = True).astype('int')
+        # Set Bar
+        if stat == 1:
+            # Set plt
+            self.setPlt('19대 대통령 선거기간(2017-04-18 ~ 2017-05-09)동안 언급된 키워드(TOP 10)')
+            plt.bar(data['KEYWORD'], data['COUNT'])
+        else:
+            # Set plt1
+            plt.subplot(2, 1, 1)
+            plt.title('19대 대통령 선거기간(2017-04-18 ~ 2017-05-09)동안 후보(TOP 5) 언급률', fontsize = 30)
+            plt.bar(data['KEYWORD'], data['COUNT'] / data['COUNT'].sum() * 100, width = 0.2)
+            plt.ylabel('COUNT(%)')
+            # Set plt2
+            plt.subplot(2, 1, 2)
+            plt.title('후보(TOP 5)의 득표율', fontsize = 30)
+            plt.bar(data['KEYWORD'], data['COUNT_'] / data['COUNT_'].sum() * 100, width = 0.2)
+            plt.ylabel('COUNT(%)')
+        #Graph Output
+        plt.get_current_fig_manager().full_screen_toggle()
         plt.show()
 # --------------------------------------------------------------------------------------------------
 # test
