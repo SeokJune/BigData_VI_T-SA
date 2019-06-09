20efe2d1fe683953f8315acdfe4cba24fedaf0df
SeokJune <46333532+SeokJune@users.noreply.github.com>
2019-06-09 17:43:47 +0900

    Update Analysis_Visual.py

--- 02.Code/01.Python/Analysis_Visual.py
+++ 02.Code/01.Python/Analysis_Visual.py
@@ -23,6 +23,7 @@ import pyecharts
 # --------------------------------------------------------------------------------------------------
 #  Class Name: Analysis_Visual
 # Method list: Generator
+#            : preprocessData, setPlt, setLabel, autoLabel
 #            : wordCloud
 #            : line, bar, stackedBar, pie
 # --------------------------------------------------------------------------------------------------
@@ -45,24 +46,63 @@ class Analysis_Visual:
         # Set Hangle Font, size / Graph size
         font_name = font_manager.FontProperties(fname = self.hPath).get_name()
         rc('font', family = font_name)
+        rc('figure', figsize = (10, 5))
+    # ----------------------------------------------------------------------------------------------
+    # Preprocessing 
+    # ----------------------------------------------------------------------------------------------
+    def preprocessData(self, kCountData, stat = 0):
+        # Preprocessing
+        data = pd.DataFrame(list(kCountData), columns = ['KEYWORD', 'COUNT']).sort_values(['COUNT'], ascending = [False])
+        if stat == 1:
+            data = data.head(10)
+        else:
+            data = data[data['KEYWORD'].isin(['문재인', '홍준표', '안철수', '유승민', '심상정'])]
+            data = pd.merge(data.sort_values(['KEYWORD']),
+                            np.transpose(self.realVoteData)[0]['문재인':'심상정'].sort_index(),
+                            left_on = ['KEYWORD'], right_index = True)
+            data = data.rename(columns = {0 : 'COUNT_'})
+            data['COUNT_'] = data['COUNT_'].replace(',', '', regex = True).astype('int')
+        return data
     # ----------------------------------------------------------------------------------------------
     # Set matplotlib Param
     # ----------------------------------------------------------------------------------------------
-    def setPlt(self, title, titleSize):
+    def setPlt(self, title, titleSize = 40, textNum = 0):
         # Set Title
         plt.suptitle(title, fontsize = titleSize)
         # Set Text(Date, Query)
         fromDate = '-'.join([self.date[0][0:4], self.date[0][4:6], self.date[0][6:8]])
-        toDate = '-'.join([self.date[1][0:4], self.date[1][4:6], self.date[1][6:8]])
+        toDate = '-'.join([self.date[1][0:4], self.date[1][4:6], str(int(self.date[1][6:8]) - 1).zfill(2)])
         if textNum == 1:
             plt.text(10 * 2.54 * 15.5, -10, '(기간: ' + fromDate + ' ~ ' + toDate + ')', ha = 'right', wrap=True, fontsize = 20, color = 'red')
             plt.text(10 * 2.54 * 15.5, -1, '(쿼리: ' + self.query + ')', ha = 'right', wrap=True, fontsize = 20, color = 'red')
+    # ----------------------------------------------------------------------------------------------
+    # Set X, Y label
+    # ----------------------------------------------------------------------------------------------
+    def setLabel(self, xl, yl):
+        # Set X Label
+        plt.xlabel(xl, fontsize = 20)
+        # Set X Label
+        plt.ylabel(yl, fontsize = 20)
+    # ----------------------------------------------------------------------------------------------
+    # Set X Value
+    # ----------------------------------------------------------------------------------------------
+    def autoLabel(self, ax, rects, xpos = 'center'):
+        ha = {'center': 'center', 'right': 'left', 'left': 'right'}
+        offset = {'center': 0, 'right': 1, 'left': -1}
+
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                         xy = (rect.get_x() + rect.get_width() / 2, height),
+                         xytext = (offset[xpos] * 3, 3),
+                         textcoords = "offset points",
+                         ha = ha[xpos], va = 'bottom')
     # ----------------------------------------------------------------------------------------------
     # Word Cloud with kCountData(TWITTER - KEYWORD_COUNT)
     # ----------------------------------------------------------------------------------------------
     def wordCloud(self, kCountData):
         # Set plt
-        self.setPlt('19대 대통령 선거 Word Count 결과', 50)
+        self.setPlt('19대 대통령 선거 Word Count 결과', 40, 1)
         # Set Word Cloud
         wc = WordCloud(font_path = self.hPath,      # Font Path
                        background_color = 'white',  # Backgroud Color
@@ -83,13 +123,13 @@ class Analysis_Visual:
     # ----------------------------------------------------------------------------------------------
     def line(self, sJsonData):
         # Preprocessing
-        data = pd.merge(pd.merge(pd.merge(pd.merge(pd.DataFrame(list(sJsonData[0]), columns = ['DATE','문재인']),
-                                                   pd.DataFrame(list(sJsonData[1]), columns = ['DATE','홍준표']), on = 'DATE'),
-                                          pd.DataFrame(list(sJsonData[2]), columns = ['DATE','안철수']), on = 'DATE'),
-                                 pd.DataFrame(list(sJsonData[3]), columns = ['DATE','유승민']), on = 'DATE'),
-                        pd.DataFrame(list(sJsonData[4]), columns = ['DATE','심상정']), on = 'DATE')
+        data = pd.merge(pd.merge(pd.merge(pd.merge(pd.DataFrame(list(sJsonData[0]), columns = ['DATE', '문재인']),
+                                                   pd.DataFrame(list(sJsonData[1]), columns = ['DATE', '홍준표']), on = 'DATE'),
+                                          pd.DataFrame(list(sJsonData[2]), columns = ['DATE', '안철수']), on = 'DATE'),
+                                 pd.DataFrame(list(sJsonData[3]), columns = ['DATE', '유승민']), on = 'DATE'),
+                        pd.DataFrame(list(sJsonData[4]), columns = ['DATE', '심상정']), on = 'DATE')
         # Set plt
-        self.setPlt('19대 대통령 선거기간(2017-04-16 ~ 2017-05-09)동안 일별 후보 언급 횟수', 40, 2)
+        self.setPlt('19대 대통령 선거기간(2017-04-18 ~ 2017-05-09)동안 일별 후보 언급 횟수')
         # Set Line
         plt.plot(data['DATE'], data['문재인'], lw = 2, marker = 'o')
         plt.plot(data['DATE'], data['홍준표'], lw = 2, marker = 'o')
@@ -97,6 +137,9 @@ class Analysis_Visual:
         plt.plot(data['DATE'], data['유승민'], lw = 2, marker = 'o')
         plt.plot(data['DATE'], data['심상정'], lw = 2, marker = 'o')
         plt.legend(('문재인', '홍준표', '안철수', '유승민', '심상정'), loc = 'upper right')
+        plt.grid()
+        # Set Label
+        self.setLabel('Date', 'Count')
         #Graph Output
         plt.get_current_fig_manager().full_screen_toggle()
         plt.show()
@@ -105,36 +148,38 @@ class Analysis_Visual:
     # ----------------------------------------------------------------------------------------------
     def bar(self, kCountData, stat = 0):
         # Preprocessing
-        data = pd.DataFrame(list(kCountData), columns = ['KEYWORD', 'COUNT']).sort_values(['COUNT'], ascending = [False])
-        if stat == 1:
-            data = data.head(10)
-        else:
-            data = data[data['KEYWORD'].isin(['문재인', '홍준표', '안철수', '유승민', '심상정'])]
-            data = pd.merge(data.sort_values(['KEYWORD']),
-                            np.transpose(self.realVoteData)[0]['문재인':'심상정'].sort_index(),
-                            left_on = ['KEYWORD'], right_index = True)
-            data = data.rename(columns = {0 : 'COUNT_'})
-            data['COUNT_'] = data['COUNT_'].replace(',', '', regex = True).astype('int')
+        data = self.preprocessData(kCountData, stat)
         # Set Bar
         if stat == 1:
             # Set plt
             self.setPlt('19대 대통령 선거기간(2017-04-18 ~ 2017-05-09)동안 언급된 키워드(TOP 10)')
             plt.bar(data['KEYWORD'], data['COUNT'])
+            # Set Label
+            self.setLabel('Keyword', 'Count')
         else:
-            # Set plt1
-            plt.subplot(2, 1, 1)
-            plt.title('19대 대통령 선거기간(2017-04-18 ~ 2017-05-09)동안 후보(TOP 5) 언급률', fontsize = 30)
-            plt.bar(data['KEYWORD'], data['COUNT'] / data['COUNT'].sum() * 100, width = 0.2)
-            plt.ylabel('COUNT(%)')
-            # Set plt2
-            plt.subplot(2, 1, 2)
-            plt.title('후보(TOP 5)의 득표율', fontsize = 30)
-            plt.bar(data['KEYWORD'], data['COUNT_'] / data['COUNT_'].sum() * 100, width = 0.2)
-            plt.ylabel('COUNT(%)')
+            # the x locations for the groups
+            ind = np.arange(len(data['KEYWORD']))
+            # the width of the bars
+            width = 0.35
+            # Set ax
+            fig, ax = plt.subplots()
+            p1 = ax.bar(ind - width / 2, round(data['COUNT'] / data['COUNT'].sum() * 100, 3), width, label = '트윗 언급')
+            p2 = ax.bar(ind + width / 2, round(data['COUNT_'] / data['COUNT_'].sum() * 100, 3), width, label = '실제 득표')
+            # title
+            ax.set_title('\n\n\n\n', fontsize = 5)
+            # label
+            ax.set_xlabel('Keyword', fontsize = 20)
+            ax.set_ylabel('Count(%)', fontsize = 20)
+            # x axis
+            ax.set_xticks(ind)
+            ax.set_xticklabels(data['KEYWORD'])
+            ax.legend()
+            self.autoLabel(ax, p1, 'left')
+            self.autoLabel(ax, p2, 'right')
+            # add layout
+            fig.tight_layout()
+            # title
+            self.setPlt('후보 TOP5 트윗 언급횟수(%) 및 실제 득표수(%)')
         #Graph Output
         plt.get_current_fig_manager().full_screen_toggle()
         plt.show()
-# --------------------------------------------------------------------------------------------------
-# test
-# https://zzsza.github.io/development/2018/08/24/data-visualization-in-python/
-# --------------------------------------------------------------------------------------------------
